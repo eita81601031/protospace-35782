161ec8e00e4f42456e1e27e567d8f527175c5a4b
eita81601031 <85558700+eita81601031@users.noreply.github.com>
2021-08-22 14:24:18 +0900

    プロトタイプを削除できるよう修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -34,8 +34,7 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
-    if prototype.destroy
+    if @prototype.destroy
        redirect_to root_path
     end
   end

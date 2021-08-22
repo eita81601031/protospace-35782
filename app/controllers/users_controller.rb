7c8dd80d400c4b3a0006a16f039009ee63186611
eita81601031 <85558700+eita81601031@users.noreply.github.com>
2021-08-22 14:14:08 +0900

    詳細ページへ遷移するよう修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @name = current_user.name
-    @prototype = current_user.prototypes
-    @prototypes = Prototype.all
   end
 end
